929ee566a3a54f59e7b82e1cf109009ea7228130
Eric Loudon <loudon.eric@gmail.com>
2019-07-22 13:11:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and i speak #{language}"
+  puts "Hi my name is #{name} and i speak #{language}."
 end
