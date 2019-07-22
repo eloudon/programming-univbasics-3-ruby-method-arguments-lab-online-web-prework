2f928300d43e3efed79214669b6a08120e550550
Eric Loudon <loudon.eric@gmail.com>
2019-07-22 13:27:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,6 +11,6 @@ def introduction_with_language_optional(name, language)
 end
 
 def introduction_with_language_optional(name, language)
-  language = Ruby
+  language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
